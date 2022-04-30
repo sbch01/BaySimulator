e2b18827ef10456bd71d4d47f8978af5ff24671e
banchev2001 <banchev2001@abv.bg>
2022-04-30 19:02:46 +0300

    First step

--- BaySimulator.ino
+++ BaySimulator.ino
@@ -1,10 +1,25 @@
 /*
-  CB Simulator
+  BaySimolator is open source device wich simulate 1 circuit braker and 4 switches.
+  This tool is used for simulating in lab conditions behaivor of primariy equipment. During relay protection test.
+  
 */
+
+
 #include <Arduino.h>
 #include <LiquidCrystal.h>
 //#include <EEPROM.h>
 #include <USBAPI.h>
+
+
+/* Ports names
+   Именуване на входовете и изходите
+=======================================================*/
+
+const uint8_t CB_closeCmd = 2; 
+const uint8_t CB_openCmd = 3; 
+const uint8_t CB_state = 14; 
+const uint8_t CB_ready = 15; 
+
 
 class CircuitBraker {
 
@@ -14,20 +29,35 @@ int  MotorTime;
 
 };
 
-// the setup function runs once when you press reset or power the board
+// SETUP INICIALISATION
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
+
+  /* Config direction of ports
+     Конфигуриране на посоката портовете  */
   pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(PIN_A0,OUTPUT);
-  pinMode(PIN_A1,OUTPUT);
+  
+  pinMode(CB_closeCmd,INPUT);
+  pinMode(CB_openCmd,INPUT);
+
+  pinMode(CB_state,OUTPUT);
+  pinMode(CB_ready,OUTPUT);
+
+  /* Off All switches and circuit braker */
+  digitalWrite(CB_state, LOW);
+  digitalWrite(CB_ready, LOW);
 }
 
-// the loop function runs over and over again forever
+// LOOO...P.............................
+//======================================
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
-  digitalWrite(PIN_A0,HIGH);
-  delay(500);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  digitalWrite(PIN_A0,LOW);
-  delay(500);                       // wait for a second
+
+   if (!digitalRead(CB_closeCmd))
+      digitalWrite(CB_state, HIGH);
+      
+   if (!digitalRead(CB_openCmd))
+      digitalWrite(CB_state, LOW);  
+
+
+/* 
+  delay(500);                       // wait for a second */
 }
